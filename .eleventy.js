99a3d28845220cc2f698dd89411d5ae08d337749
Anish Jagdale (A.J) <76979541+root-aj000@users.noreply.github.com>
2025-07-08 14:17:38 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Watch changes in these folders
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("./src/style/style.css");
 
   return {
     dir: {
